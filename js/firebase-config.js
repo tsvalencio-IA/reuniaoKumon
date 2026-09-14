6c802803b3ff610660ad8cee7b71dcd2475b932a
tsvalencio-IA <tsvalencio@gmail.com>
2026-09-14 12:12:50 -0300

    feat: centraliza configuracao Firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+window.KUMON_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCaFH2nn5VD9eTAdCcdkduDxfg8BbU-dM8",
+  authDomain: "reuniaokumon.firebaseapp.com",
+  projectId: "reuniaokumon",
+  storageBucket: "reuniaokumon.firebasestorage.app",
+  messagingSenderId: "672456540538",
+  appId: "1:672456540538:web:ffcb6eeb8338bb13d796c3",
+  databaseURL: "https://reuniaokumon-default-rtdb.firebaseio.com"
+};
