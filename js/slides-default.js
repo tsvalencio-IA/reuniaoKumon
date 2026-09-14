8bc9e435bdc52a6187290942b5cd539cb2b292fd
tsvalencio-IA <tsvalencio@gmail.com>
2026-09-14 12:14:27 -0300

    refactor: prepara base ordenada dos slides

--- js/slides-default.js
+++ js/slides-default.js
@@ -1,4 +1,3 @@
 window.KUMON_DEFAULT_SLIDES = [
-  { id:'capa', type:'cover', t:'REUNIÃO DE PAIS 2026', sub:'KUMON RIO PRETO SHOPPING', ext:'Bem-vindos!' },
-  { id:'frase1', type:'quote', text:'Obter máximo resultado com o mínimo de esforço. Produzir os melhores resultados no menor espaço de tempo é possível.', author:'Filosofia Toru Kumon' }
+  { id:'capa', type:'cover', t:'REUNIÃO DE PAIS 2026', sub:'KUMON RIO PRETO SHOPPING', ext:'Bem-vindos!' }
 ];
